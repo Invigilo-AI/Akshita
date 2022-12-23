10fa07068fea125bc9b20e3a6a89244d13aa413d
Akshita Verma <60403682+aku134@users.noreply.github.com>
2022-12-23 17:20:45 +0530

    Proper indentation added

--- megatable/megatable.py
+++ megatable/megatable.py
@@ -37,23 +37,22 @@ my_bucket = s3.Bucket('megatabletest')
 :input: folder - folder name that is to be uploaded on S3
 :output: Uploads the folder in the S3 bucket
 '''
-def upload(folder):
-    #replace annotations by suitable folder name
-    for i in range(2,11):
-        if(os.path.exists(f'{i}annotations')):
-            for file in os.listdir(f'{i}annotations'):
-                if(file):
 
+def upload(folder):
+    # replace annotations by suitable folder name
+    for i in range(2, 11):
+        if (os.path.exists(f'{i}annotations')):
+            for file in os.listdir(f'{i}annotations'):
+                if (file):
                     s3.Bucket('megatabletest').upload_file(
-                        Filename=f'{i}annotations/'+file, Key='annotations/'+file)
+                        Filename=f'{i}annotations/' + file, Key='annotations/' + file)
     print("file uploaded")
 
 
 # Insert data into db
 def insert():
-  
-    ann=[]
-    img=[]
+    ann = []
+    img = []
     for my_bucket_object in my_bucket.objects.all():
         url = s3_client.generate_presigned_url(
             ClientMethod='get_object',
@@ -63,42 +62,32 @@ def insert():
                 'Key': my_bucket_object.key
             }
         )
-        # insert(url,id)
-
-
         url = url.split("?")[0]
 
-        if (url.split("/",4)[3] == 'images'):
-
+        if (url.split("/", 4)[3] == 'images'):
             img.append(url)
 
-
-        if(url.split("/",4)[3]=='annotations'):
-            if (url =='https://megatabletest.s3.amazonaws.com/annotations/'):
+        if (url.split("/", 4)[3] == 'annotations'):
+            if (url == 'https://megatabletest.s3.amazonaws.com/annotations/'):
                 continue
 
-            f_url=url
-
+            f_url = url
             ann.append((f_url))
 
-    img_name=[]
-    ann_name=[]
+    img_name = []
+    ann_name = []
     for i in range(len(img)):
-        name=img[i].split("/",4)[4].split(".")[0]
-
+        name = img[i].split("/", 4)[4].split(".")[0]
         img_name.append(name)
-
 
     for i in range(len(ann)):
         name = ann[i].split("/", 4)[4].split(".")[0]
         ann_name.append(name)
 
-    id=2445
-    d=1
+    id = 2445
     for i in range(len(ann)):
         if ann_name[i] in img_name:
-
-            fid=f'f0{id}'
+            fid = f'f0{id}'
             # cursor.execute('''
             # insert into frame
             # (frame_id,frame_url) values
@@ -113,14 +102,9 @@ def insert():
             video_url,
             frame_metadata_json) values
             (%s,%s,%s,%s,%s,%s,%s,%s)
-
-            ''',(fid,date(2022,12,15),1002,2,ann[i],"True","",""))
-
-
+            ''', (fid, date(2022, 12, 15), 1002, 2, ann[i], "True", "", ""))
 
             id += 1
-            d+=1
-
 
 # create schema
 def create():
@@ -135,13 +119,10 @@ def create():
     frame_metadata_json varchar(65535))
     ''')
 
-
 # Delete data into db
 def delete(table_name):
     id = input("Enter frame_id you want to delete")
     cursor.execute('''DELETE FROM FRAME WHERE frame_id= %s ''', (id,))
-
-
 
 # for downloading images/annotations
 '''
@@ -149,21 +130,19 @@ def delete(table_name):
        Examples:
        1.Select frame_url from frame;
        -fetch frame_urls from frame table
-       
+
        2.Select bounding_boxes_url from training;
        -fetch annotations from training table
-       
+
        3.Select f.frame_url from 
        training as t inner join frame as f 
        on t.frame_id=f.frame_id 
        where t.artifact_type=1;
        -fetch frame_urls that are of artifact_type=2 (Hook)
-
 :output: downloads the frames or annotations in a zip file
-
 '''
 def download(query):
-    ans=[]
+    ans = []
     cursor.execute(f'''{query}''')
     result = cursor.fetchall();
     print(result)
@@ -174,32 +153,27 @@ def download(query):
     zipfile_path = zipfile_name
     zipf = zipfile.ZipFile(zipfile_path, 'w', zipfile.ZIP_DEFLATED)
 
-
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-    i=0
-    while(i<len(ans)):
+    i = 0
+    while (i < len(ans)):
 
-            url = ans[i].split("?")[0]
+        url = ans[i].split("?")[0]
 
-            file_name=url.split("/",4)[4]
-            try:
-                r = requests.get(ans[i], stream=True)
-                with open(os.path.join(folder_name,file_name), 'wb') as fd:
-                    for chunk in r.iter_content(chunk_size=128):
-                        fd.write(chunk)
-                zipf.write(os.path.join(folder_name,file_name))
-            except:
-                print("something went wrong")
-            fd.close()
-            i+=1
-
-
+        file_name = url.split("/", 4)[4]
+        try:
+            r = requests.get(ans[i], stream=True)
+            with open(os.path.join(folder_name, file_name), 'wb') as fd:
+                for chunk in r.iter_content(chunk_size=128):
+                    fd.write(chunk)
+            zipf.write(os.path.join(folder_name, file_name))
+        except:
+            print("something went wrong")
+        fd.close()
+        i += 1
 
     shutil.rmtree("downloads")
     zipf.close()
-
-
 
 
 # cursor.execute('''SELECT *
@@ -222,13 +196,13 @@ def download(query):
 #
 #     delete(table_name)
 # else:
- # view(table_name)
+# view(table_name)
 
 
 # create()
-#insert()
+# insert()
 
-query=input()
+query = input()
 download(query)
 
 conn.commit()
